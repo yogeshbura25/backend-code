88eea1f6dacfe295ce0769d207895740aa5bc007
Yogesh <burayogesh78@gmail.com>
2024-10-03 10:34:45 +0530

    second commit

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
-
-import express from "express";
-
+const express = require("express");
 
 const app = express();
 const PORT = 3004;
 
-
-
-
-
 app.get("/", (req, res) => {
-    res.send("Hello, You have achieved, yeh..!");
+  res.send("Hello, You have achieved, yeh..!");
 });
-
-
 
 app.listen(PORT, () => {
-    console.log(`Server is running successfully on port ${PORT}`);
+  console.log(`Server is running successfully on port ${PORT}`);
 });
-
-
-export default app;
