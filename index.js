7d1fd0f52ef39e1c058c7e7aa049e6d37c39494f
Yogesh <burayogesh78@gmail.com>
2024-10-03 10:38:55 +0530

    third commit

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const express = require("express");
+import express from "express";  // Import the express framework
+import { Router } from "express";  // Import the Router function from express
 
-const app = express();
-const PORT = 3004;
+const app = express();  // Initialize the express app
+const PORT =  3004;  // Set the port
 
-app.get("/", (req, res) => {
+const router = Router();  // Create a router instance
+
+router.get("/", (req, res) => {
   res.send("Hello, You have achieved, yeh..!");
 });
 
+// Attach the router to the app
+app.use(router);
+
+// Start the server to listen on the specified port
 app.listen(PORT, () => {
   console.log(`Server is running successfully on port ${PORT}`);
 });
